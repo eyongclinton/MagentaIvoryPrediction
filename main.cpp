71acbd3a6b8f8e797d9f4c10bb8f1a4931f2e07c
eyongclinton <150918279+eyongclinton@users.noreply.github.com>
2024-03-20 01:47:51 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!\n";
+  std::cout << "Hello World!\n";
+  double radius=5.0;
+ double pi = 3.14;
+  double circumference = 2 * pi * radius;;
+  std::string name = "Josh doe";
+  std::cout<< "you are: " << name;
+  std::cout<< "the circumference is: " << circumference;
+}
